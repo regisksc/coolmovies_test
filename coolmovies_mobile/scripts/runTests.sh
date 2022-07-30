e89ed1e3958744367b920d020b50eb24a578ab76
Regis Kian <regisksc@gmail.com>
2022-07-30 19:07:23 -0300

    fixes cd command

--- coolmovies_mobile/scripts/runTests.sh
+++ coolmovies_mobile/scripts/runTests.sh
@@ -5,7 +5,7 @@ sudo apt-get update
 sudo apt-get -y install lcov
 flutter pub global activate remove_from_coverage
 echo '############################### Running tests ################################'
-cd ./coolmovies_mobile
+cd ..
 flutter test --coverage
 echo '############################### Removing generated files #####################'
 flutter pub global run remove_from_coverage:remove_from_coverage -f coverage/lcov.info -r 'lib/core/error*' -r 'lib/core/transition*' -r 'navigator_service.dart'
