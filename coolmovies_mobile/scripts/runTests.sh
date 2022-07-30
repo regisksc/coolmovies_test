2f52ef8b82d0b9f3fc9dd5400cf7f1774e840a2c
Regis Kian <regisksc@gmail.com>
2022-07-30 19:11:00 -0300

    refactors build.yml

--- coolmovies_mobile/scripts/runTests.sh
+++ coolmovies_mobile/scripts/runTests.sh
@@ -6,6 +6,7 @@ sudo apt-get -y install lcov
 flutter pub global activate remove_from_coverage
 echo '############################### Running tests ################################'
 cd ..
+ls
 flutter test --coverage
 echo '############################### Removing generated files #####################'
 flutter pub global run remove_from_coverage:remove_from_coverage -f coverage/lcov.info -r 'lib/core/error*' -r 'lib/core/transition*' -r 'navigator_service.dart'
