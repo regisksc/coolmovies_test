3f2930f5c76e83ef6948735dcbfdb990bbd02a54
Regis Kian <regisksc@gmail.com>
2022-07-30 15:16:31 -0300

    ci: trigger

--- coolmovies_mobile/scripts/runTests.sh
+++ coolmovies_mobile/scripts/runTests.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-cd ..
 echo '############################### Set up #######################################'
 sudo apt-get update
 sudo apt-get -y install lcov
 flutter pub global activate remove_from_coverage
 echo '############################### Running tests ################################'
+cd ./coolmovies_mobile
 flutter test --coverage
-ls
 echo '############################### Removing generated files #####################'
 flutter pub global run remove_from_coverage:remove_from_coverage -f coverage/lcov.info -r 'lib/core/error*' -r 'lib/core/transition*' -r 'navigator_service.dart'
 echo '############################### Generating coverage ##########################'
