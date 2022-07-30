a8aa52bd581efb01f87a601c0994f6442722a04c
Regis Kian <regisksc@gmail.com>
2022-07-30 15:58:13 -0300

    ci: trigger

--- coolmovies_mobile/scripts/runCodeMetrics.sh
+++ coolmovies_mobile/scripts/runCodeMetrics.sh
@@ -3,5 +3,5 @@
 cd ..
 echo '############################### Running code metrics #######################################'
 flutter pub global activate dart_code_metrics 
-flutter pub global run dart_code_metrics:metrics lib --reporter=html
+flutter pub global run dart_code_metrics:metrics coolmovies_mobile/lib --reporter=html
 echo '############################### Code metrics generated under metrics directory #############'
