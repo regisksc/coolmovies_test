68a8069a739e03b6af2a277105fd0fb07bc6f191
Regis Kian <regisksc@gmail.com>
2022-07-30 19:44:58 -0300

    Update runTests.sh

--- coolmovies_mobile/scripts/runTests.sh
+++ coolmovies_mobile/scripts/runTests.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 echo '############################### Set up #######################################'
+echo "ls before"
+ls
 cd ..
+echo "ls after"
+ls
 sudo apt-get update
 sudo apt-get -y install lcov
 flutter pub global activate remove_from_coverage
 echo '############################### Removing generated files #####################'
-flutter pub global run remove_from_coverage:remove_from_coverage -f coverage/lcov.info -r 'lib/core/error*' -r 'lib/core/transition*' -r 'navigator_service.dart'
+flutter pub global run remove_from_coverage:remove_from_coverage -f coverage/lcov.info
 echo '############################### Generating coverage ##########################'
 genhtml coverage/lcov.info -o coverage/html
